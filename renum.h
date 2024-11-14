366ae1b2f35e7bcbd2be16855847fcc92ee3fd41
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-11-14 18:43:33 +0900

    add comments

--- renum.h
+++ renum.h
@@ -5,18 +5,38 @@
 
 #include <string>
 
-#ifndef RENUM_LINENO_START
-    #define RENUM_LINENO_START 10
-#endif
-#ifndef RENUM_LINENO_STEP
-    #define RENUM_LINENO_STEP 10
-#endif
+#define RENUM_LINENO_START 10
+#define RENUM_LINENO_STEP 10
 
-typedef unsigned long renum_lineno_t;
-typedef int renum_error_t;
+typedef unsigned long renum_lineno_t;   // Line number
+typedef int renum_error_t;              // Error code
 
+/**
+ * @brief Displays the version of the renum program.
+ */
 void RENUM_version(void);
+
+/**
+ * @brief Displays the usage information for the renum program.
+ */
 void RENUM_usage(void);
 
+/**
+ * @brief Adds line numbers to a BASIC program text.
+ * @param text The BASIC program text to modify.
+ * @param start The starting line number (default: 10).
+ * @param step The increment step between lines (default: 10).
+ * @param force Force renumbering even if an invalid line number is encountered.
+ * @return Error code (0 for success).
+ */
 renum_error_t RENUM_add_line_numbers(std::string& text, renum_lineno_t start = RENUM_LINENO_START, renum_lineno_t step = RENUM_LINENO_STEP, bool force = false);
+
+/**
+ * @brief Renumbers the lines of a BASIC program text.
+ * @param text The BASIC program text to modify.
+ * @param start The starting line number (default: 10).
+ * @param step The increment step between lines (default: 10).
+ * @param force Force renumbering even if an invalid line number is encountered.
+ * @return Error code (0 for success).
+ */
 renum_error_t RENUM_renumber_lines(std::string& text, renum_lineno_t start = RENUM_LINENO_START, renum_lineno_t step = RENUM_LINENO_STEP, bool force = false);
